d3387ffd7d7a74aee68c1c7d236de68ad13e0825
lifuling <lflchengxu@163.com>
2018-09-09 21:17:28 +0800

    调整BasePresenter逻辑  添加Obserber的统一管理

--- app/src/main/java/baseproject/com/mybaseproject/mvp/presenter/BasePresenterImpl.kt
+++ app/src/main/java/baseproject/com/mybaseproject/mvp/presenter/BasePresenterImpl.kt
@@ -1,6 +1,12 @@
 package baseproject.com.mybaseproject.mvp.presenter
 
 import baseproject.com.mybaseproject.mvp.contract.IContract
+import baseproject.com.mybaseproject.net.ResultObserver
+import baseproject.com.mybaseproject.utils.scheduler.RxUtils
+import io.reactivex.CompletableObserver
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableObserver
 
 /**
  * Created by wm on 2018/9/8.
@@ -12,6 +18,9 @@ import baseproject.com.mybaseproject.mvp.contract.IContract
  */
  open class BasePresenterImpl<V : IContract.IBaseView> : IContract.IBasePresenter<V> {
 
+    //用于事件源的统一管理   订阅和取消订阅
+    private  var mCompositeDisposable = CompositeDisposable()
+
     protected var mView: V? = null
 
     override fun attachView(view: V) {
@@ -19,8 +28,40 @@ import baseproject.com.mybaseproject.mvp.contract.IContract
     }
 
     override fun detachView() {
+        if (mCompositeDisposable != null){
+            mCompositeDisposable.clear()
+        }
         mView = null
     }
 
+    /**
+     * 获取视图引用
+     */
+    open fun getView() : V? {
+        return mView
+    }
 
+    /**
+     * 是否已经绑定视图
+     */
+    open fun isAttached() : Boolean{
+        return mView != null
+    }
+
+    /**
+     * 订阅的统一管理
+     */
+    open fun <T> addDisposable( disposableObserver: DisposableObserver<T>){
+        if (mCompositeDisposable == null){
+            mCompositeDisposable = CompositeDisposable()
+        }
+        mCompositeDisposable.add(disposableObserver)
+    }
+
+    open fun <T> cancelDisposable(disposableObserver: DisposableObserver<T>){
+        if (mCompositeDisposable == null){
+            return
+        }
+        mCompositeDisposable.remove(disposableObserver)
+    }
 }
--- app/src/main/java/baseproject/com/mybaseproject/mvp/presenter/LoginPresenterImpl.kt
+++ app/src/main/java/baseproject/com/mybaseproject/mvp/presenter/LoginPresenterImpl.kt
@@ -1,11 +1,41 @@
 package baseproject.com.mybaseproject.mvp.presenter
 
+import baseproject.com.mybaseproject.model.bean.User
 import baseproject.com.mybaseproject.mvp.contract.LoginContract
+import baseproject.com.mybaseproject.net.ApiErrorModel
+import baseproject.com.mybaseproject.net.ResultObserver
+import baseproject.com.mybaseproject.net.RetrofitManager
+import baseproject.com.mybaseproject.utils.ToastUtils
+import baseproject.com.mybaseproject.utils.scheduler.RxUtils
 
 class LoginPresenterImpl : BasePresenterImpl<LoginContract.LoginView>() , LoginContract.LoginPresenter{
 
     override fun login() {
         mView?.loginSucess("登录成功")
+
+        var params = HashMap<String , String>()
+
+        var callback: ResultObserver<User> = object : ResultObserver<User>() {
+            override fun success(data: User) {
+                mView?.hideProgress()
+                mView?.loginSucess(data.toString())
+            }
+
+            override fun failure(statusCode: Int, apiErrorModel: ApiErrorModel) {
+                ToastUtils.showToast(apiErrorModel.toString())
+                mView?.hideProgress()
+                mView?.loginFailed(apiErrorModel.message)
+            }
+        }
+
+        RetrofitManager.service.login(params)
+                .compose(RxUtils.io2main())
+                .subscribe(callback)
+
+        addDisposable(callback)
+
     }
+
+
 
 }
